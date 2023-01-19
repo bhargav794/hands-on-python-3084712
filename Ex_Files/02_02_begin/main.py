a7ebc03a2e8e0fee61e6714a2ed63ca91c1b909c
bhargav avinash <66371186+bhargav794@users.noreply.github.com>
2023-01-19 07:45:33 +0000

    changed 02_02

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -2,11 +2,16 @@ greet = "Hello World"
 extened_grt = "Hello World, " + "this is a long string"
 
 name = "John"
+doon = "max"
 
 intrupution = f"Hello {name}"
+disruption = f"Hii {doon}"
 
 greet_format = "Hello {}"
+greet_formats = "Hello p{}p jjjj" 
 
 formatted = greet_format.format(name)
+formattedext = greet_formats.format(doon)
 
 print(intrupution, formatted)
+print(formattedext)
