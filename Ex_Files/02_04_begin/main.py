de6a0b8f4ad7480941d6e83a1f2855463c5196e1
bhargav avinash <66371186+bhargav794@users.noreply.github.com>
2023-01-19 09:39:03 +0000

    ch

--- Ex_Files/02_04_begin/main.py
+++ Ex_Files/02_04_begin/main.py
@@ -2,14 +2,15 @@ NAMES = ["John", "Paul", "George", "Ringo"]
 AGES = [20, 21, 22, 23]
 
 i = 0
+"""
 while i < len(NAMES):
     print(NAMES[i], AGES[i])
     i += 1
-
+"""
 for name in NAMES:
     print(name)
 
-for name, age in zip(NAMES, AGES):
+for name, age in zip(NAMES, AGES): #zips both the lists
     print(f"{name} {age}")
 
 for name in reversed(NAMES):
@@ -19,3 +20,5 @@ for i in range(5):
     print(i)
 
 # enumerate
+for i,names in enumerate(NAMES):
+    print(f"{i} {names}")
