f28e8236a6dcf1824e43d78f0b803cb952f3107b
bhargav avinash <66371186+bhargav794@users.noreply.github.com>
2023-01-19 09:15:54 +0000

    added comments for slicing

--- Ex_Files/02_03_begin/main.py
+++ Ex_Files/02_03_begin/main.py
@@ -4,10 +4,11 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
-GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
-EVERY_OTHER = NAMES[::2]
+JOHN_PAUL = NAMES[:2] #slices the string after first two
+GEORGE_RINGO = NAMES[2:] #keeps first two
+REVERSE = NAMES[::-1] # reverses the list
+EVERY_OTHER = NAMES[::2] #jumps to every alternate index
+#slicing syntax arrname[start:end:step] ex: names[:2], names[::-1]
 
 print(sum(AGES))
 print(min(AGES))
